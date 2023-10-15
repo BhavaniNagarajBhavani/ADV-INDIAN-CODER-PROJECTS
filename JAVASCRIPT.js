9b38d04a3e3e1d22afb56a1e0568a1336024bb30
BhavaniNagarajBhavani <128892231+BhavaniNagarajBhavani@users.noreply.github.com>
2023-10-15 12:33:32 +0530

    Add files via upload

---
+++ JAVASCRIPT.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".multiple-text", {
+    strings: [
+        "FONTEND DEVELOPER" , 
+    "GRAPHIC DESIGNER"
+],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backdelay: 1000,
+    loop: true,
+});
